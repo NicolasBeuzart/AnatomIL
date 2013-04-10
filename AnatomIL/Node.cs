312d6a1b490d8857a94689d72b3d2d37123a9bcd
Corentin <broux@intechinfo.fr>
2013-04-10 09:42:39 +0200

    Add class Stack, Node and StackTest

---
+++ AnatomIL/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnatomIL
+{
+    class Node
+    {
+        public Node Next;
+        public object Value;
+
+        public Node(object value)
+              :this(value, null)
+        {
+        }
+
+        public Node(object value, Node next)
+        {
+            Next = next;
+            Value = value;
+        }
+    }
+}
---
+++ AnatomIL/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnatomIL
+{
+    public class Stack
+    {
+        private Node first = null;
+        private int nbr_elt = 0;
+
+
+        public object Pop()  // Return first element from stack and delete it from stack
+        {
+            if (first == null)
+            {
+                throw new InvalidOperationException("Empty Stack");
+            }
+            else
+            {
+                object tmp = first.Value;
+                first = first.Next;
+                nbr_elt--;
+                return tmp;
+            }
+        }
+
+        public void Push(object elt) // Add element on stack
+        {
+            first = new Node(elt, first);
+            nbr_elt++;
+        }
+
+        public bool IsEmpty  // Return TRUE if the stack is empty, else FALSE
+        {
+            get
+            {
+                return (first == null);
+            }
+        }
+
+        public int Count  // Return number of elements in stack
+        {
+            get
+            {
+                return nbr_elt;
+            }
+        }
+
+    }
+}
