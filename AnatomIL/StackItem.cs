ea02af0c9ca422cfde69203830b4c60d4a8094fa
Corentin <broux@intechinfo.fr>
2013-04-11 14:30:46 +0200

    Add class StackItem
    Modif on class Stack

---
+++ AnatomIL/StackItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnatomIL
+{
+    class StackItem
+    {
+        public Type t;
+        public object Value;
+    }
+}
