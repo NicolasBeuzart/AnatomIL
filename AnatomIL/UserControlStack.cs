91ded91fa9e77fbed9dfac0e3585b9ab1516317f
Nico <nbeuzart@intechinfo.fr>
2013-05-07 10:04:43 +0200

    ajout stackitemframe dans le windows form

--- AnatomIL/UserControlStack.cs
+++ AnatomIL/UserControlStack.cs
@@ -23,6 +23,7 @@ namespace AnatomIL
         {
             listboxStack.Items.Clear();
             string[] s = new string[ButtonsBar.CurrentComputer.Stack.Count];
+            List<string> s1 = new List<string>();
             int i = 0;
 
             foreach (StackItem StIt in ButtonsBar.CurrentComputer.Stack.CurrentStack)
@@ -30,6 +31,7 @@ namespace AnatomIL
                 if (StIt is StackItemValue)
                 {
                     s[ButtonsBar.CurrentComputer.Stack.CurrentStack.Count - i - 1] = StIt.Type.ToString().Split('.')[StIt.Type.ToString().Split('.').Count() - 1] + " : " + StIt.Value.ToString();
+                    s1.Add(StIt.Type.ToString().Split('.')[StIt.Type.ToString().Split('.').Count() - 1] + " : " + StIt.Value.ToString());
                     i++;
                 }
                 else if (StIt is StackItemFrame)
@@ -41,24 +43,30 @@ namespace AnatomIL
                     int NumberLocals = 0;
 
                     string BaseFrame = StIt.RetType.ToString().Split('.')[StIt.RetType.ToString().Split('.').Count() - 1] + " : " + StIt.FrameName;
-
+                    s1.Add(BaseFrame);
                     foreach(StackItemValue args in StIt.Args)
                     {
                         ListArgs += "A" + NumberArgs.ToString() + " --> " + args.Type.ToString() + " : " + args.Value.ToString() + "\r";
+                        
+                        s1.Add("A" + NumberArgs.ToString() + " --> " + args.Type.ToString() + " : " + args.Value.ToString());
                         NumberArgs++;
                     }
 
                     foreach (StackItemValue locvars in StIt.VarLocals)
                     {
-                        ListLocalsVars += "L" + NumberLocals.ToString() + " --> " + locvars.Type.ToString() + " : " + locvars.Value.ToString() + "\r";
+                        ListLocalsVars += "L" + NumberLocals.ToString() + " --> " + locvars.Type.ToString().Split('.')[StIt.Type.ToString().Split('.').Count() - 1] + " : " + locvars.Value.ToString() + "\r";
+                        
+                        s1.Add("L" + NumberLocals.ToString() + " --> " + locvars.Type.ToString().Split('.')[StIt.Type.ToString().Split('.').Count() - 1] + " : " + locvars.Value.ToString());
                         NumberLocals++;
                     }
 
                     s[ButtonsBar.CurrentComputer.Stack.CurrentStack.Count - i - 1] = ListLocalsVars + ListArgs + BaseFrame;
+
                     i++;
                 }
             }
-            listboxStack.Items.AddRange(s);
+            s1.Reverse();
+            listboxStack.Items.AddRange(s1.ToArray());
         }
 
     }
